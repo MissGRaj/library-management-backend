f31290ed7de7760daf942460ad5e6d1b443e7f41
athul <athul@athuls-MacBook-Pro.local>
2026-09-18 18:56:31 +0530

    Swagger UI Auth Bearer changes

--- src/main/java/com/example/library_management/controller/BorrowingController.java
+++ src/main/java/com/example/library_management/controller/BorrowingController.java
@@ -4,6 +4,7 @@ import com.example.library_management.dto.request.BorrowingRequest;
 import com.example.library_management.dto.response.BorrowingResponse;
 import com.example.library_management.entity.Borrowing;
 import com.example.library_management.service.BorrowingService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/borrowings")
+@SecurityRequirement(name = "bearerAuth")
 public class BorrowingController {
 
     private final BorrowingService borrowingService;
